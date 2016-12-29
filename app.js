357f13cb0f3916b3f057ed9953f6d0f77c6af6dc
Prabhat Sharma <hi.prabhat@gmail.com>
2016-12-29 07:07:42 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var restify = require('restify');
+
+var os = require("os");
+var hostname = os.hostname();
+
+function respond(req, res, next) {
+  res.send('server ' + hostname + 'says, hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
